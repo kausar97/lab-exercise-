2769714b9e840e0e599eb8ee3160695100cdd114
Kausar <84766439+kausar97@users.noreply.github.com>
2021-06-07 15:48:41 +0530

    Add files via upload

---
+++ CheckEvenOdd.java
@@ -0,0 +1,18 @@
+
+public class CheckEvenOdd {
+
+	public static void main(String[] args)
+	{
+		
+		int num=20;
+		/* if number is divisible by 2 then it's an even number 
+		 * else odd number*/
+		if ( num % 2 == 0)
+			System.out.println("Entered number is even");
+		else
+			System.out.println("Entered number is odd");
+		
+
+	}
+
+}
